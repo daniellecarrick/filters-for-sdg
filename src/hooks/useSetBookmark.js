1ba9cd97d36c4f2ac2cfb4af5c5626e5178c3ea3
John Bellizzi <jbellizzi@axisgroup.com>
2019-09-24 16:10:51 -0400

    adding setBookmark hook

---
+++ src/hooks/useSetBookmark.js
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from "react"
+import { useSession } from "dash-component-library/context"
+import { combineLatest, switchMap } from "rxjs/operators"
+import { Subject } from "rxjs"
+
+export default ({ app } = {}) => {
+	const sessions = useSession()
+	const {
+		rxq: { doc$ },
+	} = app ? sessions.find(session => session.name === app) : sessions[0]
+
+	const set$ = useRef(new Subject()).current
+
+	useEffect(() => {
+		const sub$ = set$
+			.pipe(
+				combineLatest(doc$),
+				switchMap(([bookmarkId, docHandle]) =>
+					docHandle.ask("ApplyBookmark", bookmarkId)
+				)
+			)
+			.subscribe()
+
+		return () => sub$.unsubscribe()
+	}, [doc$])
+
+	const setBookmark = id => set$.next(id)
+
+	return { setBookmark }
+}
