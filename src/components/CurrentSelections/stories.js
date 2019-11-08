b02c1c21f7f964946e62fe92d2e411156d3d06ef
John Bellizzi <jbellizzi@axisgroup.com>
2019-11-07 21:33:58 -0500

    CurrentSelections documentation

---
+++ src/components/CurrentSelections/stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import CurrentSelections from ".";
+
+export default { title: "Current Selections" };
+
+export const basic = () => <div>Current Selections Story in progress</div>;
