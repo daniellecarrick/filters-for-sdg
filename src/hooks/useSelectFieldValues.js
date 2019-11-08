17e132059b5b1297893947724949b8559c5cb336
John Bellizzi <jbellizzi@axisgroup.com>
2019-11-07 20:24:39 -0500

    fixed hook references

--- src/hooks/useSelectFieldValues.js
+++ src/hooks/useSelectFieldValues.js
@@ -1,42 +1,42 @@
-import { useEffect, useRef } from "react"
-import { useSession } from "dash-component-library/context"
-import { combineLatest, map, switchMap, retry } from "rxjs/operators"
-import { Subject } from "rxjs"
-import { qAskReplayRetry } from "../operators"
+import { useEffect, useRef } from "react";
+import { useSession } from "../context";
+import { combineLatest, map, switchMap, retry } from "rxjs/operators";
+import { Subject } from "rxjs";
+import { qAskReplayRetry } from "../operators";
 
 export default ({ field, app }) => {
-	const sessions = useSession()
-	const {
-		rxq: { doc$ },
-	} = app ? sessions.find(session => session.name === app) : sessions[0]
+  const sessions = useSession();
+  const {
+    rxq: { doc$ },
+  } = app ? sessions.find(session => session.name === app) : sessions[0];
 
-	const select$ = useRef(new Subject()).current
+  const select$ = useRef(new Subject()).current;
 
-	useEffect(() => {
-		const field$ = doc$.pipe(qAskReplayRetry("GetField", field))
+  useEffect(() => {
+    const field$ = doc$.pipe(qAskReplayRetry("GetField", field));
 
-		const sub$ = select$
-			.pipe(
-				map(values => {
-					const isNumeric =
-						values.find(value => typeof value === "string") === undefined
+    const sub$ = select$
+      .pipe(
+        map(values => {
+          const isNumeric =
+            values.find(value => typeof value === "string") === undefined;
 
-					return values.map(value => ({
-						[isNumeric ? "qNumber" : "qText"]: value,
-						qIsNumeric: isNumeric,
-					}))
-				}),
-				combineLatest(field$),
-				switchMap(([selections, fieldHandle]) =>
-					fieldHandle.ask("SelectValues", selections, false).pipe(retry(3))
-				)
-			)
-			.subscribe()
+          return values.map(value => ({
+            [isNumeric ? "qNumber" : "qText"]: value,
+            qIsNumeric: isNumeric,
+          }));
+        }),
+        combineLatest(field$),
+        switchMap(([selections, fieldHandle]) =>
+          fieldHandle.ask("SelectValues", selections, false).pipe(retry(3))
+        )
+      )
+      .subscribe();
 
-		return () => sub$.unsubscribe()
-	}, [field, doc$])
+    return () => sub$.unsubscribe();
+  }, [field, doc$]);
 
-	const select = values => select$.next(values)
+  const select = values => select$.next(values);
 
-	return { select }
-}
+  return { select };
+};
