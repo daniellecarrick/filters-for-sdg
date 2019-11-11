32d3e2fbe2e6941a3bdc57b2883e78eaaac2b45f
John Bellizzi <jbellizzi@axisgroup.com>
2019-11-11 17:33:36 -0500

    adding to ClearButton story

--- src/components/ClearButton/stories.js
+++ src/components/ClearButton/stories.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ClearButton from ".";
+import { SessionProvider } from "../../context";
+import { qlikConfig } from "../../config";
 
 export default { title: "Clear Button" };
 
-export const basic = () => <div>Clear Button Story in progress</div>;
+export const basic = () => (
+  <SessionProvider qlikConfig={qlikConfig}>
+    <ClearButton />
+  </SessionProvider>
+);
---
+++ src/config/qlik-config.js
@@ -0,0 +1,7 @@
+export default {
+  name: "Ad Ops",
+  host: "dash.condenast.com",
+  isSecure: true,
+  port: 443,
+  appname: "04688823-dc09-4ea6-8ea3-ee0fb33774a1",
+};
