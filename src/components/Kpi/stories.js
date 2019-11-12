08d78bde4d0c52c3c680479274cfc5dd44e16440
John Bellizzi <jbellizzi@axisgroup.com>
2019-11-12 11:16:15 -0500

    kpi stories

--- src/components/Kpi/stories.js
+++ src/components/Kpi/stories.js
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Kpi from ".";
+import { SessionProvider, useSession } from "../../context";
+import { adOpsConfig } from "../../config";
+import { qAskReplayRetry } from "../../operators";
+import { invalidations } from "rxq";
 
 export default { title: "Kpi" };
 
 export const basic = () => <Kpi label="My KPI">$12.4M</Kpi>;
+
+export const qlik = () => (
+  <SessionProvider qlikConfig={adOpsConfig}>
+    <QlikComp />
+  </SessionProvider>
+);
+
+const QlikComp = () => {
+  const {
+    rxq: { doc$ },
+  } = useSession()[0];
+
+  const [KPI, setKPI] = useState(null);
+
+  useEffect(() => {
+    const sub$ = doc$
+      .pipe(
+        qAskReplayRetry("CreateSessionObject", {
+          qInfo: { qType: "kpi" },
+          kpi: {
+            qValueExpression: `sum({<Type={Curr},LineType={Direct},Rollup={*}-{'ignore','Native','video','CM','native'}>}Impressions)/sum({<Type={Curr},LineType={Capacity},Rollup={*}-{'ignore','Native','video','CM','native'}>}Impressions)`,
+          },
+        }),
+        invalidations(true),
+        qAskReplayRetry("GetLayout")
+      )
+      .subscribe(layout => {
+        setKPI(layout.kpi);
+      });
+
+    return () => sub$.unsubscribe();
+  }, [doc$]);
+
+  return <Kpi label="Direct">{KPI}</Kpi>;
+};
