e71f32505972129a1261a701c19d6451bc88fb8f
John Bellizzi <jbellizzi@axisgroup.com>
2019-11-12 11:04:24 -0500

    CurrentSelections story

--- src/components/CurrentSelections/index.js
+++ src/components/CurrentSelections/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import withStyles from "react-jss";
 import { useCurrentSelections } from "../../hooks";
+import classNames from "classnames";
 
-const styles = {};
+const styles = {
+  currentSelections: {
+    fontFamily: "Rubik !important",
+  },
+};
+
 export default withStyles(styles)(({ className, classes }) => {
   const currentSelections = useCurrentSelections();
 
   return currentSelections !== null && currentSelections !== "-" ? (
-    <div className={className}>{currentSelections}</div>
+    <div className={classNames(className, classes.currentSelections)}>
+      {currentSelections}
+    </div>
   ) : null;
 });
