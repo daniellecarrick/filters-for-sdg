7b2b8c10043a0c12a92a84db0640ef4db1d59585
John Bellizzi <jbellizzi@axisgroup.com>
2019-11-14 14:09:32 -0500

    prettier

--- src/components/QdtComponent/stories.js
+++ src/components/QdtComponent/stories.js
@@ -4,9 +4,18 @@ import { SessionProvider } from "../../context";
 import { adOpsConfig } from "../../config";
 
 export default { title: "QDT Component" };
+// const prefix = window.location.pathname.substr(
+//   0,
+//   window.location.pathname.toLowerCase().lastIndexOf("/extensions")
+// );
+const prefix = "/dev";
 
+const qlikConfigWithPrefix = {
+  ...adOpsConfig,
+  prefix,
+};
 export const basic = () => (
-  <SessionProvider qlikConfig={adOpsConfig}>
+  <SessionProvider qlikConfig={qlikConfigWithPrefix}>
     <QdtComponent
       type="QdtViz"
       qdtProps={{
