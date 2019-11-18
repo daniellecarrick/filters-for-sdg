347c2b2851ef8b3215bf14ab207d5e0bc11d248a
John Bellizzi <jbellizzi@axisgroup.com>
2019-11-18 15:15:32 -0500

    adding storybook docs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,16 +1,16 @@
-import { addParameters, configure } from "@storybook/react";
+import { configure, addParameters } from "@storybook/react";
 import "../src/styles/global.css";
 
-// addParameters({
-//   options: {
-//     storySort: (a, b) => a[1].id.localeCompare(b[1].id),
-//   },
-// });
+addParameters({
+  options: {
+    storySort: (a, b) => a[1].id.localeCompare(b[1].id),
+  },
+});
 
 configure(
   [
-    require.context("../src", true, /\.stories\.js$/),
-    require.context("../src", true, /\.stories\.mdx$/),
+    require.context("../src", true, /stories\.js$/),
+    require.context("../src", true, /stories\.mdx$/),
   ],
   module
 );
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import classNames from "classnames";
 import withStyles from "react-jss";
 
@@ -35,26 +36,41 @@ const styles = {
   },
 };
 
-export default withStyles(styles)(
-  ({
-    theme = "dark", // Icon = null,
-    size = "sm",
-    onClick = () => {},
-    className,
-    classes,
-    children,
-  }) => {
-    return (
-      <button
-        className={classNames("button", classes.button, className, {
-          [classes.button_dark]: theme === "dark",
-          [classes.button_light]: theme === "light",
-          [classes.button_lg]: size === "lg",
-        })}
-        onClick={onClick}
-      >
-        {children}
-      </button>
-    );
-  }
-);
+const Button = ({
+  theme, // Icon = null,
+  size,
+  onClick,
+  className,
+  classes,
+  children,
+}) => {
+  return (
+    <button
+      className={classNames("button", classes.button, className, {
+        [classes.button_dark]: theme === "dark",
+        [classes.button_light]: theme === "light",
+        [classes.button_lg]: size === "lg",
+      })}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  /** color theme of button */
+  theme: PropTypes.oneOf(["dark", "light"]),
+  /** size of button */
+  size: PropTypes.oneOf(["sm", "lg"]),
+  /** Function run when button clicked */
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  theme: "dark",
+  size: "sm",
+  onClick: () => {},
+};
+
+export default withStyles(styles)(Button);
