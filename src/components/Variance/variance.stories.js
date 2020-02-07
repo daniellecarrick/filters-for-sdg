8e9eba32ba76e7f0cf18e882bd63fe3715585348
Danielle Carrick <danielle_carrick@condenast.com>
2020-02-07 16:09:39 -0500

    seperating out postiive and negativ versions

--- src/components/Variance/variance.stories.js
+++ src/components/Variance/variance.stories.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Variance from ".";
 export default { title: "Variance", component: Variance };
-export const basic = () => (
+
+export const negative = () => (
   <>
     <Variance newValue={4800} oldValue={6000} />
+  </>
+);
+
+export const positive = () => (
+  <>
     <Variance newValue={8800} oldValue={6000} />
   </>
 );
