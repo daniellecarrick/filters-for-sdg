cdf86f78f0b9ff0a6e72c8d23f5f72f88dc29cd2
Jason Daniel <jason_daniel@condenast.com>
2020-02-19 18:22:22 +0530

    merging with master

--- src/config/index.js
+++ src/config/index.js
@@ -2,14 +2,7 @@ import {
   adOpsConfig,
   consumerMarketingConfig,
   socialConfig,
-<<<<<<< HEAD
-  cmExecDashConfig,
-} from "./qlik-config";
-
-export { adOpsConfig, consumerMarketingConfig, socialConfig, cmExecDashConfig };
-=======
   affiliateConfig,
 } from "./qlik-config";
 
 export { adOpsConfig, consumerMarketingConfig, socialConfig, affiliateConfig };
->>>>>>> master
