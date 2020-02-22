d8e196ab0a9b30807e8dee28c670f92bcc019c78
Danielle Carrick <dacarrick@gmail.com>
2020-02-21 19:50:55 -0500

    merged in develop branch and dealt with merge conflicts

--- src/config/index.js
+++ src/config/index.js
@@ -2,14 +2,14 @@ import {
   adOpsConfig,
   consumerMarketingConfig,
   socialConfig,
-<<<<<<< HEAD
   affiliateConfig,
-} from "./qlik-config";
-
-export { adOpsConfig, consumerMarketingConfig, socialConfig, affiliateConfig };
-=======
   cmExecDashConfig,
 } from "./qlik-config";
 
-export { adOpsConfig, consumerMarketingConfig, socialConfig, cmExecDashConfig };
->>>>>>> origin/develop
+export {
+  adOpsConfig,
+  affiliateConfig,
+  consumerMarketingConfig,
+  socialConfig,
+  cmExecDashConfig,
+};
