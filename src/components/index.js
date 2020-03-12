fdc3b8bcc21e95f4c2d9864328feea1e56364e2c
Jason Daniel <jason_daniel@condenast.com>
2020-03-12 10:21:36 +0530

    adding CneKpi to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ export { default as BrandSelector } from "./BrandSelector";
 export { default as Button } from "./Button";
 export { default as ButtonGroup } from "./ButtonGroup";
 export { default as Card } from "./Card";
+export { default as CneKpi } from "./CneKpi";
 export { default as ClearButton } from "./ClearButton";
 export { default as CurrentSelections } from "./CurrentSelections";
 export { default as DashLogo } from "./DashLogo";
